db5076a1dba6df8da8afb06c60077006602ef652
LaylaAbab <LaylaAbab@users.noreply.github.com>
2022-01-20 13:02:16 +0300

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author l_A_Y
+ */
+public class Main {
+    static void main(String[] args){
+        System.out.print("Hello World");
+    }
+}
